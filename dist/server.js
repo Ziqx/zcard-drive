2688dbad105b1db116f99bcb8705b4407dc28cd4
fathah-work <abdulfathahkodag@gmail.com>
2024-08-12 12:47:01 +0400

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,37 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// src/server.ts
+var import_nextdrive = __toESM(require("nextdrive"));
+var import_dotenv = require("dotenv");
+var import_path = __toESM(require("path"));
+(0, import_dotenv.config)({ path: import_path.default.resolve(__dirname, "../.env") });
+var server = new import_nextdrive.default(
+  {
+    port: 4002,
+    apiKey: process.env.API_KEY,
+    folder: "drive"
+  }
+);
+server.start();
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import NextDriveServer from 'nextdrive';
+import { config } from 'dotenv';
+import path from 'path';
+
+config({ path: path.resolve(__dirname, '../.env') }); 
+
+const server = new NextDriveServer(
+    {
+    port:4002,
+    apiKey: process.env.API_KEY!,
+    folder: 'drive'});
+server.start();
